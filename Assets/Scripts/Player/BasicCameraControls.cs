d0e291f6d2e02175531475451ca5f985fdcd5f9c
Antoine T-S <anto291100@gmail.com>
2021-05-27 19:26:32 -0400

    setup de camera

---
+++ Assets/Scripts/Player/BasicCameraControls.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCameraControls : ICameraControls
+{
+    public float sensitivity = 1;
+    public void MoveCamera(GameObject head)
+    {
+        
+    }
+}
