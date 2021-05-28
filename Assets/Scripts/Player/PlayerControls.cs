02519a8568dffa04bdf1b87c148abe9e078c19d1
Memoh <CraftMan3512@gmail.com>
2021-05-28 00:11:18 -0400

    back to not invokerepeating

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -6,13 +6,12 @@ public class PlayerControls : MonoBehaviour
 {
     private IPlayerControls controlsScript;
     public GameObject head;
-
-    private bool invokeStarted = false;
     // Start is called before the first frame update
     void Start()
     {
 
         controlsScript = new BasicPlayerControls(head, GetComponent<Rigidbody>());
+      
 
     }
 
@@ -27,26 +26,19 @@ public class PlayerControls : MonoBehaviour
     private void FixedUpdate()
     {
 
-        if (!invokeStarted)
+        if (Client.instance != null)
         {
             
-            InvokeRepeating(nameof(SendPlayerPosToServer),0,0.033333f);
-            invokeStarted = true;
+            SendPlayerPosToServer();//TODO PROGRAMMER CETTE FONCTION LA
 
         }
-        
+
     }
 
     private void SendPlayerPosToServer()
     {
-
-        if (Client.instance != null)
-        {
-            
-            ClientSend.PlayerPosition(GetComponent<Rigidbody>().position);  
-            Debug.Log("A");
-            
-        }
-
+        
+        ClientSend.PlayerPosition(transform.position);
+        
     }
 }
