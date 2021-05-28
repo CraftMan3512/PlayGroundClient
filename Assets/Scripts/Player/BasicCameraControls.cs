19519b5d28251e26514b1064727c2385326d8aa1
Antoine T-S <anto291100@gmail.com>
2021-05-27 21:51:53 -0400

    first person workingish

--- Assets/Scripts/Player/BasicCameraControls.cs
+++ Assets/Scripts/Player/BasicCameraControls.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class BasicCameraControls : ICameraControls
 {
-    public float sensitivity = 1;
+    public float sensitivity = 5;
     public void MoveCamera(GameObject head)
     {
-        
+        head.transform.Rotate(-Input.GetAxis("Mouse Y")*sensitivity, 0, 0);
+        head.transform.Rotate(0, Input.GetAxis("Mouse X")*sensitivity, 0);
+        head.transform.eulerAngles = new Vector3(Clampcamera(head.transform.eulerAngles.x), head.transform.eulerAngles.y, 0);
+
+    }
+
+    private float Clampcamera(float x)
+    {
+
+        if (x > 90)
+        {
+
+            return Mathf.Clamp(x, 271, 360);
+
+        }
+        else
+        {
+
+            return Mathf.Clamp(x, 0, 89);
+
+        }
+
     }
 }
--- Assets/Scripts/Player/CameraControls.cs
+++ Assets/Scripts/Player/CameraControls.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
+   
+    private ICameraControls cameraControl;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraControl = new BasicCameraControls();
+        ToggleCursor(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cameraControl.MoveCamera(gameObject);
+    }
+
+    public void ToggleCursor(bool toggled)
+    {
+        Cursor.visible = toggled;
     }
 }
