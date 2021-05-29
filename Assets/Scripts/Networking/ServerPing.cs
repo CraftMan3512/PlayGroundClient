0326d6bcb0447406f82f62b9107e4250a3dec936
Memoh <CraftMan3512@gmail.com>
2021-05-29 10:43:02 -0400

    Instant ping!!!!!! no lag :)

--- Assets/Scripts/Networking/ServerPing.cs
+++ Assets/Scripts/Networking/ServerPing.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
@@ -7,16 +9,25 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Ping = System.Net.NetworkInformation.Ping;
 
 public class ServerPing : MonoBehaviour
 {
-
+    private static ServerPing instance;
+    
     public TMP_InputField ip;
 
     public Image connectivityIndicator;
 
     public Button playButton;
-    
+
+    private void Awake()
+    {
+
+        instance = this;
+
+    }
+
     public void ConnectToServer()
     {
         
@@ -26,8 +37,7 @@ public class ServerPing : MonoBehaviour
 
     public async void PingServer()
     {
-
-        Debug.Log("PINGING SERVER!");
+        
         if (await PingHost())
         {
             
@@ -45,21 +55,63 @@ public class ServerPing : MonoBehaviour
         }
         
     }
-
+    
     private async Task<bool> PingHost()
     {
         try
         {
-            using (var client = new TcpClient(ip.text, Client.port))
+
+            //parse localhost
+            string sendIp = string.Equals(ip.text, "localhost", StringComparison.CurrentCultureIgnoreCase) ? "127.0.0.1" : ip.text;
+
+            //ping machine first, if it doesnt work, don't ping the port to save the freeze
+            using (Ping pingSender = new Ping())
             {
-                await Task.Delay(10);
-                return true;   
-                
+
+                var options = new PingOptions();
+
+                options.DontFragment = true;
+
+                // Create a buffer of 32 bytes of data to be transmitted.
+                byte[] buffer = Encoding.ASCII.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+                //send ping async
+                PingReply reply = pingSender.Send(IPAddress.Parse(sendIp), 80, buffer, options);
+
+                if (reply.Status == IPStatus.Success)
+                {
+
+                    //success, ping port now
+                    using (var client = new TcpClient())
+                    {
+                        client.SendTimeout = 60;
+                        client.ReceiveTimeout = 60;
+                        await client.ConnectAsync(sendIp, Client.port);
+                        return true;
+
+                    }
+
+                }
+                else
+                {
+
+                    throw new Exception("Could not resolve IP address, there is no server at this IP.");
+
+                }
+
             }
+
         }
-        catch (SocketException ex)
+        catch (FormatException ex)
         {
-            Debug.Log("Error pinging host:'" + ip.text + ":" + Client.port + "'");
+            
+            Debug.Log("U didnt enter an ip address >:(");
+            return false;
+
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Error pinging host:'" + ip.text + ":" + Client.port + $"', ex: {ex}");
             return false;
         }
     }
