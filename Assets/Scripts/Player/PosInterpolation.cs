0d50f47f815b4a938afe190f8d5bc4056b5f49f6
Memoh <CraftMan3512@gmail.com>
2021-06-03 21:00:35 -0400

    test new movement interpolation

--- Assets/Scripts/Player/PosInterpolation.cs
+++ Assets/Scripts/Player/PosInterpolation.cs
@@ -7,21 +7,11 @@ using UnityEngine;
 public class PosInterpolation : MonoBehaviour
 {
 
-    public enum InterpolationTypes
-    {
-        
-        Lerp, Slerp
-        
-    }
-    
-    public InterpolationTypes interpolationMode = InterpolationTypes.Lerp;
-
 
     public int nbPos;
     private float startTime;
-    private bool firstPos = true;
     private Vector3 PreviousPos;
-    private Queue<Vector3> Movements;
+    private Vector3 nextPos;
     private Transform tf;
 
     private void Awake()
@@ -29,68 +19,14 @@ public class PosInterpolation : MonoBehaviour
         
         tf = transform;
         startTime = 0.1f;
-        Movements = new Queue<Vector3>();
         PreviousPos = tf.position;
 
     }
+
     private void Update()
     {
-
-        if (Movements.Count > 0)
-        {
-
-            double fraction = (Time.time - startTime) / Time.fixedDeltaTime;
-            
-            ErrorDisplayer.Log($"TRYING TO MOVE OTHER PLAYER, FRACTION IS {fraction}, {Movements.Count} POS INSIDE");
-
-            if (fraction <= 1f && !firstPos)
-            {
-                
-                switch (interpolationMode)
-                {
-
-                    case InterpolationTypes.Lerp:
-
-                        tf.position = Vector3.Lerp(PreviousPos,Movements.Peek(), (float)fraction);
-    
-                    break;
-                    case InterpolationTypes.Slerp:
-
-                        tf.position = Vector3.Slerp(PreviousPos, Movements.Peek(), (float)fraction);
-    
-                    break;
-                    
-                }
-                
-            }
-            else
-            {
-
-                if (firstPos)
-                {
-                    firstPos = false;
-                    PreviousPos = Movements.Dequeue();
-                }
-                else
-                {
-
-                    tf.position = Movements.Last();
-                    Movements.Clear();
-
-                }
-                NewMovement();
-
-            }
-
-        }
-
-    }
-
-    //Set time-related variables when changing movement coordinates
-    public void NewMovement()
-    {
         
-        startTime = Time.fixedTime;
+        tf.position = Vector3.MoveTowards(tf.position, nextPos, Vector3.Distance(tf.position, nextPos) * Time.fixedDeltaTime * Time.deltaTime);
         
     }
 
@@ -98,7 +34,8 @@ public class PosInterpolation : MonoBehaviour
     {
         
         //ErrorDisplayer.Log($"PLAYER INTERPOL: {movement}");
-        Movements.Enqueue(movement);
+        nextPos = movement;
+
         //ErrorDisplayer.Log($"WE HAVE {Movements.Count} MOVEMENTS IN STOCK!!!"); //TODO Enable this debug to check if the queue risks overloading sometime, seems good for now
 
     }
